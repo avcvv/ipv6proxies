c02ae93a9ecf685ec4ee0626e9dbd2904901cc9d
avcvv <39009179+avcvv@users.noreply.github.com>
2020-06-25 22:06:37 +0300

    Update 3proxycfg.sh

--- 3proxycfg.sh
+++ 3proxycfg.sh
@@ -1,7 +1,7 @@
 echo pidfile /root/3proxy/3proxy.pid
 
 echo daemon
-echo maxconn 250
+echo maxconn 3333
 echo nserver 1.1.1.1
 echo nserver [2606:4700:4700::1111]
 echo nserver [2606:4700:4700::1001]
@@ -12,13 +12,15 @@ echo timeouts 1 5 30 60 180 1800 15 60
 echo setgid 65535
 echo setuid 65535
 echo flush
-echo #auth strong
-echo #users admin:CL:pass
 echo #allow admin
-echo "auth iponly"
-echo "allow * 1.1.1.1"
 
 
+# HTTP(S) прокси
+echo "#auth iponly"
+echo "#allow * 1.4.8.8"
+echo "users user1:CL:user1"
+echo "flush"
+echo "allow * * * *"
 
 port=30000
 count=1
@@ -30,3 +32,21 @@ for i in `cat ip.list`; do
         exit
     fi
 done
+
+# Socks прокси
+# echo "#auth iponly"
+# echo "#allow * 1.4.8.8"
+# echo "users user1:CL:user1"
+# echo "flush"
+# echo "allow * * * *"
+
+# port=30000
+# count=1
+# for i in `cat ip.list`; do
+#     echo "socks -6 -p$port  -i127.0.0.1 -e$i"
+#     ((port+=1))
+#     ((count+=1))
+#     if [ $count -eq 10001 ]; then
+#         exit
+#     fi
+# done
