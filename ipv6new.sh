8c57bd1a7616b8b1286c09f06ccb177d114490ac
avcvv <39009179+avcvv@users.noreply.github.com>
2020-07-19 18:33:44 +0300

    Update and rename ipv6new to ipv6new.sh

--- ipv6new.sh
+++ ipv6new.sh
@@ -86,8 +86,8 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        #echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
-         echo "usr1/pass1/$IP4/$port/$(gen64 $IP6)"
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+         #echo "usr1/pass1/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
