ff4a7beec14c75568e62b13da7629e52f569313b
avcvv <39009179+avcvv@users.noreply.github.com>
2021-03-14 16:45:20 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -24,8 +24,8 @@ fi
 	#make -f Makefile.Linux
 	#ulimit -u unlimited -n 999999 -s 16384
 	
-	wget https://github.com/avcvv/ipv6proxies/raw/master/3proxycfg.sh
-	wget https://github.com/avcvv/ipv6proxies/raw/master/Genips.sh
+	wgeet https://github.com/avcvv/ipv6proxies/raw/master/3proxycfg.sh
+	wgeet https://github.com/avcvv/ipv6proxies/raw/master/Genips.sh
 	chmod 0755 Genips.sh
 	chmod 0755 3proxycfg.sh
 	
