d0990b6bfb953cee0ae9234361c295bba6a8d682
avcvv <39009179+avcvv@users.noreply.github.com>
2021-06-06 17:53:23 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -17,9 +17,9 @@ fi
 	read -r -p "IP who get access to this Proxies: " vIp2
 	
 
-	#yum -y groupinstall "Development Tools"
-        #yum -y install gcc zlib-devel openssl-devel readline-devel ncurses-devel wget tar dnsmasq net-tools iptables-services system-config-firewall-tui nano iptables-services
-	#git clone https://github.com/z3APA3A/3proxy.git
+	yum -y groupinstall "Development Tools"
+        yum -y install gcc zlib-devel openssl-devel readline-devel ncurses-devel wget tar dnsmasq net-tools iptables-services system-config-firewall-tui nano iptables-services
+	git clone https://github.com/z3APA3A/3proxy.git
 	cd 3proxy
 	make -f Makefile.Linux
 	ulimit -u unlimited -n 999999 -s 16384
@@ -82,14 +82,16 @@ fi
   count=1
 
   #network=2604:180:2:b93 # your ipv6 network prefix
-
+  
+  
   rnd_ip_block ()
   {
       a=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       b=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       c=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       d=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
-      echo $network:$a:$b:$c:$d >> ip.list
+      e=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
+      echo $network:$a:$b:$c:$d:$e >> ip.list #Для /64 сети требуется 4 блока:a, b, c, d #Если сеть /48, то 5 блоков, то есть + e блок через двоеточие
   }
 
   #echo "$MAXCOUNT случайных IPv6:"
@@ -113,7 +115,7 @@ fi
 
   for i in `cat ip.list`; do
       #echo "ifconfig eth0 inet6 add $i/64"
-      ifconfig eth0 inet6 add $i/64
+      ifconfig eth0 inet6 add $i/48 # Если сеть 64 то $i/64 если 48 то $i/48
   done
 
   echo ====================================
